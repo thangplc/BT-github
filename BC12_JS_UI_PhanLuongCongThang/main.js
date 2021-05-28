5642d0982c2c048a4a5854d20652ee26b7a99e51
thangplc <thangplc1412@gmail.com>
2021-05-29 00:15:17 +0700

    fix comment

--- BC12_JS_UI_PhanLuongCongThang/main.js
+++ BC12_JS_UI_PhanLuongCongThang/main.js
@@ -2,17 +2,21 @@
 Chiều cao
 Cân nặng
 */
+//  handle
+// set innerHTML text cua span bang gia tri input nhap vao
 
-
+// output
+// span can nag va span chieu cao 
 var btn = document.getElementById("btnNhapNhap");
 
 btn.onclick = function() {
+    // input
     var chieuCao = document.getElementById("ipChieuCao").value;
     var canNang = document.getElementById("ipCanNang").value;
 
 
     // console.log(chieuCao)
-
+    // handle and output
     document.getElementById("spanChieuCao").innerText = chieuCao + "cm";
     document.getElementById("spanCanNang").innerText = canNang + "kg";
 
